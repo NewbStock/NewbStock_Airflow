e4a5d028fe3bbdfbfd4a58531c52d8fecaec8221
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-08-06 15:24:50 +0900

    Feat : high_stock top3 -> top100

--- high_volatility_stock.py
+++ high_volatility_stock.py
@@ -17,14 +17,14 @@ default_args = {
 }
 
 @dag(
-    dag_id='stock_top3',
+    dag_id='high_volatility_stock',
     default_args=default_args,
     description='Fetch and process exchange rate data for beginner stock investors',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 1, 1),
     catchup=False
 )
-def stock_top3():
+def high_volatility_stock():
 
     @task(task_id="read_csv_from_s3")
     def read_csv_from_s3():
@@ -34,16 +34,16 @@ def stock_top3():
         csv_content = s3_hook.read_key(s3_key, s3_bucket)
 
         df = pd.read_csv(StringIO(csv_content))
-        top_3_codes = df['Code'].head(3).tolist()
-        return top_3_codes
+        top_100_codes = df['Code'].head(100).tolist()
+        return top_100_codes
     
     @task(task_id="fetch_csv_files")
-    def fetch_csv_files(top_3_codes):
+    def fetch_csv_files(top_100_codes):
         s3_hook = S3Hook(aws_conn_id='s3_conn')
         s3_bucket = 'team-won-2-bucket'
         local_files = []
 
-        for code in top_3_codes:
+        for code in top_100_codes:
             file_key = f'us_stock_data/history/{code}.csv'
             try:
                 csv_content = s3_hook.read_key(file_key, s3_bucket)
@@ -67,26 +67,31 @@ def stock_top3():
         
         for file_path in local_files:
             df = pd.read_csv(file_path)
-            code = df['Code'].iloc[0]
-
-            # 변동률 계산
-            df['Close'] = df['Close'].astype(float)
-            df['Prev_Close'] = df['Close'].shift(1)
-            df['Change'] = (df['Close'] - df['Prev_Close']) / df['Prev_Close'] * 100
-
-            # 변동률이 10% 이상인 날짜 찾기
-            high_volatility = df[df['Change'].abs() > 10]
-            if not high_volatility.empty:
-                high_volatility_file = f"/tmp/{code}_high_volatility.csv"
-                high_volatility.to_csv(high_volatility_file, index=False)
+            if not df.empty:
+                code = df['Code'].iloc[0]
                 
-                # S3에 업로드
-                s3_key = f'newb_data/stock_data/{code}_high_volatility.csv'
-                s3_hook.load_file(filename=high_volatility_file, key=s3_key, bucket_name=s3_bucket, replace=True)
-                logging.info(f"Successfully uploaded {s3_key} to S3")
+                # 변동률 계산
+                df['Close'] = df['Close'].astype(float)
+                df['Prev_Close'] = df['Close'].shift(1)
+                df['Change'] = (df['Close'] - df['Prev_Close']) / df['Prev_Close'] * 100
 
-    top_3_codes = read_csv_from_s3()
-    local_files = fetch_csv_files(top_3_codes)
+                # 변동률이 10% 이상인 날짜 찾기
+                high_volatility = df[df['Change'].abs() > 10]
+                if not high_volatility.empty:
+                    high_volatility_file = f"/tmp/{code}_high_volatility.csv"
+                    high_volatility.to_csv(high_volatility_file, index=False)
+                    
+                    # S3에 업로드
+                    s3_key = f'newb_data/stock_data/{code}_high_volatility.csv'
+                    s3_hook.load_file(filename=high_volatility_file, key=s3_key, bucket_name=s3_bucket, replace=True)
+                    logging.info(f"Successfully uploaded {s3_key} to S3")
+                else:
+                    logging.info(f"No high volatility days found for {code}")
+            else:
+                logging.warning(f"The file {file_path} is empty and will be skipped")
+
+    top_100_codes = read_csv_from_s3()
+    local_files = fetch_csv_files(top_100_codes)
     find_high_volatility_days(local_files)
 
-dag = stock_top3()
+dag = high_volatility_stock()
