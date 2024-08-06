b23095292631431b7420fc6a6d374e765db3b805
Ju Kyoungyeon <91823615+Kyoung-yeon99@users.noreply.github.com>
2024-08-06 16:42:04 +0900

    Add files via upload

--- get_kr_stock_data.py
+++ get_kr_stock_data.py
@@ -8,6 +8,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.amazon.aws.operators.glue import AwsGlueJobOperator
 import FinanceDataReader as fdr
 import pandas as pd
 from datetime import datetime
@@ -21,28 +22,7 @@ def get_redshift_connection():
     conn = hook.get_conn()
     conn.autocommit = True  # default는 False
     return conn.cursor()
-
-
-# S3에서 'kr_top100.csv' (오늘 시가총액 top100) 데이터 가져오기
-"""
-def get_kr_top100():
-    bucket_name = 'team-won-2-bucket'
-    input_key = 'kr_stock_data/kr_top100.csv'
-
-    s3_hook = S3Hook(aws_conn_id='s3_conn') 
-
-    try:
-        # S3에서 파일 읽기
-        kr_top100 = s3_hook.read_key(input_key, bucket_name)
-        df_kr_top100 = pd.read_csv(StringIO(kr_top100))
-        logging.info("Successfully read kr_top100.csv from S3")
-        return df_kr_top100
-    
-    except Exception as e:
-        logging.error(f"Error reading kr_top100.csv from S3: {str(e)}")
-        return 
-"""
-
+  
 
 # Redshift 테이블 kr_top100에서 distinct (name, code) 데이터 가져오기  
 def get_disticnt_data():
@@ -75,61 +55,46 @@ def check_existing_files():
         return 
 
 
-def update_redshift(update_df):
-    # redshift 연결
-    cur = get_redshift_connection()
-    
-    # 한국 주식 과거 데이터 테이블 date, name, code, open
-    
-    try:
-        # Date, Open, High, Low, Close, Volume, Change, Updown, Comp, Amount, MarCap, Shares
-        for index, row in update_df.iterrows():
-            cur.execute("""
-                INSERT INTO kr_stock_data (date, name, code, open_value)
-                VALUES (%s, %s, %s, %s)
-            """, (row['Date'].strftime('%Y-%m-%d'), row['name'], row['code'], row['Open']))
-    except Exception as e:
-        logging.error(f"Error updating Redshift {update_df['name']} ({update_df['code']}): {str(e)}")
-
-
 # S3에 시가총액 Top100 데이터 업데이트 및 생성
 def update_stock_data():
     bucket_name = 'team-won-2-bucket'
     s3_hook = S3Hook(aws_conn_id='s3_conn') 
-
-    # 한국 시장 시가총액 100위 데이터 가져오기
-    # df_kr_top100 = get_kr_top100()
-    # current_codes = set(df_kr_top100['CompanyCode'].astype(str).str.zfill(6))
     
+    # Redshift 테이블 kr_top100에서 distinct (name, code) 데이터 가져오기
     distinct_data = get_disticnt_data()
+    logging.info("length of distinct data from Redshift Table 'kr_top100': {len(distinct_data)}")
 
-    # 이미 존재하는 주식 데이터 파일 확인 
+    # S3에 이미 존재하는 주식 데이터 가져오기
     existing_codes = check_existing_files()
+    logging.info("length of exisiting codes from S3 bucket: {len(distinct_data)}")
 
     # 현재 top 100 기업의 데이터 업데이트 또는 새로 생성
     for company in distinct_data:
         company_name = company['name']
         company_code = company['code']
         key = f'kr_stock_data/stock_data/{company_code}.csv'
-        #company_code = str(company['code']).zfill(6)  # 'code' 컬럼의 값을 6자리로 맞추기 (앞에 0 추가)
 
         try:
-            if company_code in existing_codes:
-                # 기존 파일 읽어오기
+            if company_code in existing_codes: 
+                # 기존 파일 읽기
                 file_content = s3_hook.read_key(key, bucket_name)
                 existing_df = pd.read_csv(StringIO(file_content), index_col=0, parse_dates=True)
                 new_record = fdr.DataReader(f'KRX:{company_code}', datetime.today().date())  # UTC, KST 주의
-                new_record.index.name = 'Date'  # 인덱스 이름 설정
-                new_record.reset_index(inplace=True)  # 인덱스를 컬럼으로 변환
                 df = pd.concat([existing_df, new_record])            
             else:
-                df = fdr.DataReader(f'KRX:{company_code}', start = '2000-01-01')
-                df.index.name = 'Date'  # 인덱스 이름 설정
-                df.reset_index(inplace=True)  # 인덱스를 컬럼으로 변환
+                df = fdr.DataReader(f'KRX:{company_code}', start_date = '2000-01-01')        
             
+
+            df.index.name = 'Date'  # 인덱스 이름 설정
+            df.reset_index(inplace=True)  # 인덱스를 컬럼으로 변환
+
             # FinanceDataReader 컬럼
             # Date, Open, High, Low, Close, Volume, Change, Updown, Comp, Amount, MarCap, Shares
+            df['name'] = company_name
+            df['code'] = company_code
+            
             if not df.empty:
+                key = f'kr_stock_data/stock_data/{company_code}.csv'
                 csv_buffer = StringIO()
                 df.to_csv(csv_buffer, index=True)
 
@@ -137,19 +102,13 @@ def update_stock_data():
                     string_data=csv_buffer.getvalue(),
                     key=key,
                     bucket_name=bucket_name,
-                    replace=True)
-                
+                    replace=True
+                )
                 logging.info(f"Successfully saved data for {company_name} {company_code}")
-
-                # Redshift에 데이터 업데이트
-                df['name'] = company_name
-                df['code'] = company_code
-                update_redshift(df)
             else:
                 logging.info(f"Fail to get stock data for {company_name} {company_code}")
         except Exception as e:
             logging.error(f"Error processing {company_name} ({company_code}): {str(e)}")
-
 
 
 
@@ -160,12 +119,12 @@ default_args = {
 }
 
 with DAG ( 
-    dag_id='get_kr_stock_data',
+    dag_id='get_kr_marketcap_top100',
     default_args=default_args,
     description='With FinanceDataReader, download, process, and upload KOSPI Marketcap Top100 data to S3',
     schedule_interval='30 9 * * 1-5',  # UTC 09:30 (KST 18:30), 월요일부터 금요일까지 장 마감 후  
     start_date=datetime(2024, 7, 15),  # 시작 날짜
-    catchup=True,
+    catchup=False,
 ) as dag:
 
     process_and_upload_stock_data_task = PythonOperator(
@@ -173,3 +132,13 @@ with DAG (
         python_callable=update_stock_data,
         dag=dag,
     )
+
+    run_glue_job = AwsGlueJobOperator(
+        task_id='kr_stock_data_glue_job',
+        job_name='newbstock_kr_stock_data_s3_to_redshift',
+        region_name='ap-northeast-2',
+        iam_role_name='newbstock_glue_role',
+        dag=dag
+    )
+
+    process_and_upload_stock_data_task >> run_glue_job
