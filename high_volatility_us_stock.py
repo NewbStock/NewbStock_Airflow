964cc524743f15857e521109ea6af58b8aad3099
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-08-13 12:52:00 +0900

    Fix : 코드 구조 수정

--- high_volatility_us_stock.py
+++ high_volatility_us_stock.py
@@ -7,6 +7,7 @@ import pandas as pd
 import logging
 from io import StringIO
 
+# 기본 DAG 인자 설정
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -16,6 +17,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+# DAG 정의
 @dag(
     dag_id='high_volatility_us_stock',
     default_args=default_args,
@@ -28,17 +30,19 @@ def high_volatility_us_stock():
 
     @task(task_id="read_csv_from_s3")
     def read_csv_from_s3():
+        """S3에서 CSV 파일을 읽어 상위 3개의 주식 코드를 반환합니다."""
         s3_hook = S3Hook(aws_conn_id='s3_conn')
         s3_bucket = 'team-won-2-bucket'
         s3_key = 'us_stock_data/us_stock_top100.csv'
         csv_content = s3_hook.read_key(s3_key, s3_bucket)
 
         df = pd.read_csv(StringIO(csv_content))
-        top_100_codes = df['code'].head(3).tolist()
+        top_100_codes = df['code'].head(3).tolist()  # 상위 3개 주식 코드 추출
         return top_100_codes
     
     @task(task_id="fetch_csv_files")
     def fetch_csv_files(top_100_codes):
+        """상위 3개 주식 코드에 대한 CSV 파일을 S3에서 읽어 로컬에 저장합니다."""
         s3_hook = S3Hook(aws_conn_id='s3_conn')
         s3_bucket = 'team-won-2-bucket'
         local_files = []
@@ -62,13 +66,14 @@ def high_volatility_us_stock():
     
     @task(task_id="find_high_volatility_days")
     def find_high_volatility_days(local_files):
+        """로컬에 저장된 주식 데이터에서 변동성이 큰 날을 찾아 S3에 업로드합니다."""
         s3_hook = S3Hook(aws_conn_id='s3_conn')
         s3_bucket = 'team-won-2-bucket'
         
         for file_path in local_files:
             df = pd.read_csv(file_path)
             if not df.empty:
-                code = df['Code'].iloc[0]
+                code = df['code'].iloc[0]  # 주식 코드 추출
                 
                 # 변동률 계산
                 df['Close'] = df['Close'].astype(float)
@@ -83,15 +88,22 @@ def high_volatility_us_stock():
                     
                     # S3에 업로드
                     s3_key = f'newb_data/stock_data/us/{code}_high_volatility.csv'
-                    s3_hook.load_file(filename=high_volatility_file, key=s3_key, bucket_name=s3_bucket, replace=True)
+                    s3_hook.load_file(
+                        filename=high_volatility_file,
+                        key=s3_key,
+                        bucket_name=s3_bucket,
+                        replace=True
+                    )
                     logging.info(f"Successfully uploaded {s3_key} to S3")
                 else:
                     logging.info(f"No high volatility days found for {code}")
             else:
                 logging.warning(f"The file {file_path} is empty and will be skipped")
 
+    # DAG 실행 순서 정의
     top_100_codes = read_csv_from_s3()
     local_files = fetch_csv_files(top_100_codes)
     find_high_volatility_days(local_files)
 
+# DAG 인스턴스 생성
 dag = high_volatility_us_stock()
